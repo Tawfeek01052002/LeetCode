90d51b0a4e732857c05edf28a4514e46fe24e471
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-17 21:10:16 +0530

    Time: 7 ms (98.54%), Space: 15.7 MB (7.35%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
         
-        //Two Pointer Approach
+        //Two Pointer Approach---O(n)
         for(int i=0,j=arr.size()-1;i<j;){
             if(arr[i]+arr[j]==target){
                 return {i+1,j+1};
@@ -15,5 +15,33 @@ public:
             }
         }
         return {0};
+        
+        
+        //Binary Search Approach ---O(nlogn)
+        for(int i=0;i<arr.size();i++){
+            int targ=target-arr[i];
+            int index=binarySearch(arr,0,arr.size()-1,targ);
+            if(index!=i && index!=-1){
+                return {i,index};
+            }
+        }
+        
+        
+    }
+    
+    int binarySearch(vector<int> arr,int start,int end,int target){
+        while(start<end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
     }
 };
