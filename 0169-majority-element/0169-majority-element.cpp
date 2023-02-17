af18e6be06ecd4cef2c27c00958327295834b16d
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-18 02:52:37 +0530

    Time: 32 ms (16.27%), Space: 19.7 MB (33.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-        //Brute Force Approach
-        // int count=0;
-        // for(int i=0;i<nums.size();i++){
-        //     count=0;
-        //     if(nums[i]!=INT_MIN){
-        //         for(int j=i+1;j<nums.size();j++){
-        //             if(nums[i]==nums[j]){
-        //                 nums[j]=INT_MIN;
-        //                 count++;
-        //             }
-        //         }
-        //     }
-        //     if(count>=nums.size()/2){
-        //         return nums[i];
-        //     }
-        // }
-        // return 0;
-        
-        //Using Sorting ---O(nlogn)
         int n=nums.size();
         int count=1;
         sort(nums.begin(), nums.end());
@@ -33,7 +13,6 @@ public:
                 i=j;
                 count=1;
                 j++;
-                
             }
             if(count>n/2){
                 return nums[i];
