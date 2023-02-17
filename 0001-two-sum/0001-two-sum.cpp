5ebb9d79474daf08e62670421963643cfe3f0406
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-17 20:56:19 +0530

    Time: 242 ms (47.73%), Space: 10.3 MB (73.86%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        //Brute Force
+        for(int i=0;i<nums.size();i++){
+            int ele=target-nums[i];
+            for(int j=i+1;j<nums.size();j++){
+                if(ele==nums[j]){
+                    return {i,j};
+                }
+            }
+        }
+        return {0};
+    }
+};
