bbb12510983422b28c8e28f13ed8c4a17c73ff21
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-19 02:14:47 +0530

    Time: 6 ms (24.02%), Space: 8.1 MB (99.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low=0,mid=0,end=nums.size()-1;
-        while(mid<=end){
-            if(nums[mid]==1){
-                mid++;
+        
+        //Using Sort function
+        // sort(nums.begin(),nums.end());
+        
+        //Using Counting
+//         int zero=0,one=0,two=0;
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0)
+//                 zero++;
+//             else if(nums[i]==1)
+//                 one++;
+//             else
+//                 two++;
+//         }
+//         int i=0;
+//         while(zero-->0)
+//             nums[i++]=0;
+//         while(one-->0)
+//             nums[i++]=1;
+//         while(two-->0)
+//             nums[i++]=2;
+        
+        //Dutch National Flag 
+        //Using 3 Pointers
+        int l=0,m=0,h=nums.size()-1;
+        while(m<=h){
+            if(nums[m]==1){
+                m++;
             }
-            else if(nums[mid]==2){
-                swap(nums[mid],nums[end]);
-                end--;
+            else if(nums[m]==2){
+                swap(nums[m],nums[h]);
+                h--;
             }
             else{
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
+                swap(nums[l],nums[m]);
+                l++;
+                m++;
             }
         }
         
