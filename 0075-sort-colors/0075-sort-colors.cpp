a3e236226cceae414b7ff0cf9c04f3ef78f0f0c4
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-19 02:12:08 +0530

    Time: 3 ms (56.16%), Space: 8.4 MB (30.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -6,23 +6,41 @@ public:
         // sort(nums.begin(),nums.end());
         
         //Using Counting
-        int zero=0,one=0,two=0;
+//         int zero=0,one=0,two=0;
         
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0)
-                zero++;
-            else if(nums[i]==1)
-                one++;
-            else
-                two++;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0)
+//                 zero++;
+//             else if(nums[i]==1)
+//                 one++;
+//             else
+//                 two++;
+//         }
+//         int i=0;
+//         while(zero-->0)
+//             nums[i++]=0;
+//         while(one-->0)
+//             nums[i++]=1;
+//         while(two-->0)
+//             nums[i++]=2;
+        
+        //Dutch National Flag 
+        //Using 3 Pointers
+        int low=0,mid=0,end=nums.size()-1;
+        while(mid<=end){
+            if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[end]);
+                end--;
+            }
+            else{
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
         }
         
-        int i=0;
-        while(zero-->0)
-            nums[i++]=0;
-        while(one-->0)
-            nums[i++]=1;
-        while(two-->0)
-            nums[i++]=2;
     }
 };
