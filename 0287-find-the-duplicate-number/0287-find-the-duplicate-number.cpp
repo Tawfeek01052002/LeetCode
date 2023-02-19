2d02b089f0b629644c7b86c7932c93c52114ba52
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-20 00:46:09 +0530

    Time: 127 ms (62.59%), Space: 61.3 MB (68.35%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -12,19 +12,33 @@ public:
         // return -1;
             
         //Using Sorting
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(nums[i]==nums[i+1]){
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        
+        // Using Cyclic Sort
+        for(int i=0;i<n;){
+            int index=nums[i];
+            if(i==(index-1)){
+                i++;
+            }
+            else if(nums[index-1]!=nums[i]){
+                swap(nums[index-1],nums[i]);
+            }
+            else {
                 return nums[i];
             }
         }
-        return -1;
+        //2 2 2 2 2
+        //1 3 4 2 2
+        //1 3 4 2 2
+        //1 4 3 2 2 
              
-        
-        
-        
-        
-        
+        return -1;
 
     }
 };
