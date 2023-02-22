7ce18a28d057e50eb8c2ae7bb01340cbc92721a5
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-22 20:34:32 +0530

    Time: 139 ms (19.21%), Space: 59.7 MB (25.21%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,17 +4,14 @@ public:
        int s=0;
         int e=arr.size()-1;
         int m=s+(e-s)/2;
-        int index=-1;
         while(s<e){
-            if(arr[m]>arr[m+1]){
-                index=m;
+            if(arr[m]>arr[m+1])
                 e=m;
-            }
             else
                 s=m+1;
             m=s+(e-s)/2;
         }
-        return index;
+        return s;
     }
 };
 
