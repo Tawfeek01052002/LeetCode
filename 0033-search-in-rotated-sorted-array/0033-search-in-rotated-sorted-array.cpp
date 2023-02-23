b834aad3c7c0bd778847c70b408761d5401ee648
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-23 16:07:13 +0530

    Time: 3 ms (81.89%), Space: 11.2 MB (43.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    int binary(vector<int> arr,int target,int s,int e){
+    
+    //First we Found Pivot the apply binary search on left of pivot (0,pivot-1)
+    // if index ==-1 then search in right array (pivot,arr.size()-1)
+    // else return -1
+    int binarySearch(vector<int> arr,int target,int s,int e){
         int mid=s+(e-s)/2;
         while(s<=e){
             if(arr[mid]==target)
@@ -15,8 +19,9 @@ public:
         }
         return -1;
     }
-    int search(vector<int>& nums, int target) {
-        int s=0;
+    
+    int findpivot(vector<int>nums){
+         int s=0;
         int e=nums.size()-1;
         int mid=s+(e-s)/2;
         while(s<e){
@@ -26,19 +31,16 @@ public:
                 e=mid;
             mid=s+(e-s)/2;
         }
-        int pivot=s;
-        // cout<<"Pivot = "<<pivot;
-        int index=binary(nums,target,0,pivot-1);
-        if(index!=-1)
-            return index;
+        return s;
+    }
+    
+    int search(vector<int>& arr, int target) {
+        int pivot=findpivot(arr);
+        if(arr[pivot]<=target && target<=arr[arr.size()-1]){
+            return binarySearch(arr,target,pivot,arr.size()-1);
+        }
         else{
-            index=binary(nums,target,pivot,nums.size()-1);
-            if(index!=-1){
-                return index;
-            }
-            else{
-                return -1;
-            }
+            return binarySearch(arr,target,0,pivot-1);
         }
         return -1;
     }
