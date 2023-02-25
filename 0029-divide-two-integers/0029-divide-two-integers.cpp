d1fabbe0ef9cee061a6835f674a8d8ef52009cc8
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-02-25 17:31:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (62.96%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long temp=dividend;
+        if(abs(temp)>INT_MAX && abs(divisor)==1){
+            if ((dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0))
+                return INT_MAX;
+            else
+                return dividend;
+        }
+        long s = 0;
+        long e = abs(dividend);
+        long mid = s + (e - s) / 2;
+        long ans = 0;
+        while (s <= e)
+        {
+            // Perfect Solution
+            if (abs(mid * divisor) == abs(dividend))
+            {
+                ans = mid;
+                break;
+            }
+            else if (abs(mid * divisor) < abs(dividend))
+            {
+                // right search
+                ans = mid;
+                s = mid + 1;
+            }
+            else
+                e = mid - 1; // Left search
+            mid = s + (e - s) / 2;
+        }
+        if ((dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0))
+            return (int)ans;
+        else
+            return (int)-ans;
+        }
+};
