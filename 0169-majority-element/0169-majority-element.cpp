3b723d30594e1a96a2ea1db84736fad0442f169c
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-09 15:48:48 +0530

    Time: 26 ms (30.62%), Space: 19.5 MB (80.40%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,58 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int curr=0;
-        int count=0;
-        for(int num:nums){
+        
+        //Brute Force Approach
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     count=0;
+        //     if(nums[i]!=INT_MIN){
+        //         for(int j=i+1;j<nums.size();j++){
+        //             if(nums[i]==nums[j]){
+        //                 nums[j]=INT_MIN;
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        //     if(count>=nums.size()/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return 0;
+        
+        //Using Sorting ---O(nlogn)
+        // int n=nums.size();
+        // int count=1;
+        // sort(nums.begin(), nums.end());
+        // for(int i=0,j=1;i<n&&j<n;){
+        //     if(nums[i]==nums[j]){
+        //         count++;
+        //         j++;
+        //     }
+        //     else {
+        //         i=j;
+        //         count=1;
+        //         j++;
+        //     }
+        //     if(count>n/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[0];
+        
+        //Increase and Decrese
+        int major=nums[0];
+        int count=1;
+        for(int i=1;i<nums.size();i++){
             if(count==0){
-                curr=num;
                 count++;
+                major=nums[i];
             }
-            else if(num==curr)
+            else if(major==nums[i])
                 count++;
             else
                 count--;
         }
-        return curr;
+        return major;
     }
 };
