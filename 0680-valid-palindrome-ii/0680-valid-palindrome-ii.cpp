91148477045c2d8c5e2cde98ff89983c24ed7b6c
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-11 02:32:39 +0530

    Time: 64 ms (14.72%), Space: 23.3 MB (68.51%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string str){
+        int s=0;
+        int e=str.length()-1;
+        while(s<e){
+            if(str[s]!=str[e]){
+                return false;
+            }
+            s++;
+            e--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<end){
+            if(s[start]!=s[end]){
+                return isPalindrome(s.substr(start,end-start))||isPalindrome(s.substr(start+1,end-start));
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
