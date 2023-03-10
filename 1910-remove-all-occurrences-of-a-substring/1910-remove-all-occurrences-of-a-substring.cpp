ec3a526336772e60510917202712d2b8636b3148
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-11 01:29:24 +0530

    Time: 6 ms (37.58%), Space: 6.7 MB (18.32%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    //Using find function to search the starting index of the
+    // part string and removing it with erase function
     string removeOccurrences(string s, string part) {
         int n=part.length();
         int index=s.find(part);
