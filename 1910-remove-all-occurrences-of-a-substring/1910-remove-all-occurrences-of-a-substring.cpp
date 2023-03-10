6a38884552b313b4e6b8ea0e2f5b0ce03a938695
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-11 01:25:40 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (86.59%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    // Using find function to search substring if found 
-    // then remove substring using erase function
-    // of string 
     string removeOccurrences(string s, string part) {
-        int pos=s.find(part);
-        while(pos!= string::npos){
-            s.erase(pos,part.length());
-            pos=s.find(part);
+        int n=part.length();
+        while(s.find(part)!=string::npos){
+            int index=s.find(part);
+            s.erase(index,n);
         }
         return s;
     }
