9147cb5ac9c52fb55ba5bac3a71e4c045a20e0fe
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-11 23:45:39 +0530

    Time: 4 ms (79.55%), Space: 13.3 MB (37.78%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,39 +1,5 @@
 class Solution {
 public:
-    // bool isPalindrome(string str, int s, int e)
-    // {
-    //     while (s < e)
-    //     {
-    //         if (str[s] != str[e])
-    //             return false;
-    //         s++;
-    //         e--;
-    //     }
-    //     return true;
-    // }
-    
-    // Brute force approach checking all possible substring is palindrome       or not 
-    // int countSubstrings(string str1)
-    // {
-    //     int n = str1.length();
-    //     int count = 0;
-    //     for (int i = 0; i < n; i++)
-    //     {
-    //         for (int j = i; j < n; j++)
-    //         {
-    //             if (isPalindrome(str1, i, j))
-    //             {
-    //                 count++;
-    //             }
-    //             // cout << str1.substr(i, j) << endl;
-    //         }
-    //     }
-    //     return count;
-    // }
-    
-    //we are check that substring(i,j) is palindrome or not if yes
-    // then i-- and j++ again check with this
-    // two cases comes of even length substring and odd length substring
     int isPalindrome(string str,int i,int j){
         int count=0;
         int n=str.length();
@@ -49,9 +15,6 @@ public:
         }
         return count;
     }
-    
-    //Optimised Approach
-    
     int countSubstrings(string str1)
     {
         int n = str1.length();
