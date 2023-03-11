8baa15aaeeb1ff5b92c91abdef7713ed3bb49343
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-11 23:17:06 +0530

    Time: 3 ms (90.47%), Space: 13.3 MB (37.78%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,31 +1,69 @@
 class Solution {
 public:
-    bool isPalindrome(string str, int s, int e)
-{
-    while (s < e)
-    {
-        if (str[s] != str[e])
-            return false;
-        s++;
-        e--;
-    }
-    return true;
-}
-int countSubstrings(string str1)
-{
-    int n = str1.length();
-    int count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            if (isPalindrome(str1, i, j))
-            {
+    // bool isPalindrome(string str, int s, int e)
+    // {
+    //     while (s < e)
+    //     {
+    //         if (str[s] != str[e])
+    //             return false;
+    //         s++;
+    //         e--;
+    //     }
+    //     return true;
+    // }
+    
+    // Brute force approach checking all possible substring is palindrome       or not 
+    // int countSubstrings(string str1)
+    // {
+    //     int n = str1.length();
+    //     int count = 0;
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         for (int j = i; j < n; j++)
+    //         {
+    //             if (isPalindrome(str1, i, j))
+    //             {
+    //                 count++;
+    //             }
+    //             // cout << str1.substr(i, j) << endl;
+    //         }
+    //     }
+    //     return count;
+    // }
+    
+    
+    int isPalindrome(string str,int i,int j){
+        int count=0;
+        int n=str.length();
+        while(i>=0 && j<n){
+            if(str[i]==str[j]){
                 count++;
+                i--;
+                j++;
             }
-            // cout << str1.substr(i, j) << endl;
+            else{
+                return count;
+            }
         }
+        return count;
     }
-    return count;
-}
+    
+    //Optimised Approach
+    int countSubstrings(string str1)
+    {
+        int n = str1.length();
+        int count = 0;
+        for (int i = 0,j=i; i < n && j<n; i++,j++)
+        {
+            count+=isPalindrome(str1,i,j);
+        }
+        // cout<<count;
+        for (int i = 0,j=i+1; i < n && j<n; i++,j++)
+        {
+            count+=isPalindrome(str1,i,j);
+        }
+        // cout<<count;
+        return count;
+    }
+    
 };
