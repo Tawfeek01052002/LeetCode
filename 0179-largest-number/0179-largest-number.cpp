847176adaca117b7214b763117934286e6f83b9d
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-12 17:41:21 +0530

    Time: 7 ms (78.58%), Space: 11.1 MB (90.53%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,13 +1,13 @@
 
 class Solution {
 public:
-    bool operator()(int first,int second){
+    static bool cmp(int first,int second){
         string s1=to_string(first);
         string s2=to_string(second);
         return (s1+s2)>(s2+s1)?true:false;
     }
     string largestNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),Solution());
+        sort(nums.begin(),nums.end(),cmp);
         string ans="";
         for(int i=0;i<nums.size();i++)
             ans+=to_string(nums[i]);
