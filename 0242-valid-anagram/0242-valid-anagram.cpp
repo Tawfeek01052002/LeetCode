c25677f914e7e9eeaa80f7c099b9a613337a1230
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-12 17:53:35 +0530

    Time: 16 ms (33.93%), Space: 7.4 MB (17.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int arr[256]={0};
-        int sn=s.length();
-        int tn=t.length();
-        for(int i=0;i<sn;i++){
-            // cout<<(int)s[i]<<endl;
-            arr[(int)s[i]]+=1;
-            // cout<<arr[(int)s[i]]<<endl;
+        // int arr[256]={0};
+        // int sn=s.length();
+        // int tn=t.length();
+        // for(int i=0;i<sn;i++){
+        //     // cout<<(int)s[i]<<endl;
+        //     arr[(int)s[i]]+=1;
+        //     // cout<<arr[(int)s[i]]<<endl;
+        // }
+        // for(int i=0;i<tn;i++){
+        //     arr[(int)t[i]]--;
+        // }
+        // for(int i=0;i<256;i++){
+        //     if(arr[i]!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        //Using sorting T.C.=O(nlogn) S.C.=O(1)
+        if(s.length()!=t.length()){
+            return false;
         }
-        for(int i=0;i<tn;i++){
-            arr[(int)t[i]]--;
-        }
-        for(int i=0;i<256;i++){
-            if(arr[i]!=0){
-                return false;
-            }
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s.compare(t)==0?true:false;
         
     }
 };
