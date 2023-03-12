c3a83e16db93163babb5926db62728621ebfbbf1
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-12 22:42:18 +0530

    Time: 2201 ms (5.12%), Space: 6.9 MB (76.10%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,9 +3,6 @@ public:
     bool isIsomorphic(string s, string t) {
         int n=s.length();
         int m=t.length();
-        if(n!=m){
-            return false;
-        }
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(s[i]==s[j] && t[i]!=t[j] || t[i]==t[j] && s[i]!=s[j]){
