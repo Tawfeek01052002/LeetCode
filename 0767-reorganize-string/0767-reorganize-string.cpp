ebc64a9c1276182399e0f8774997b9979c0077b8
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-14 14:14:14 +0530

    Time: 5 ms (28.06%), Space: 6.2 MB (62.63%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,58 +1,6 @@
 class Solution {
 public:
-    // steps:
-    // 1. counting freq of characters
-    // 2. finding max character with count
-    // 3. put the characters with +2 diff
-    // 4. checking if max_char is not fitting in the length
-    
-    //T.C.=O(n) S.C.=O(n)
     string reorganizeString(string s) {
-//         int heap[26]={0};
-//         int n=s.length();
-//         for(int i=0;i<n;i++){
-//             heap[s[i]-'a']++;
-//         }
-        
-//         int max_char=0;
-//         int max_count=INT_MIN;
-//         for(int i=0;i<26;i++){
-//             if(heap[i]>max_count){
-//                 max_count=heap[i];
-//                 max_char=i;
-//             }
-//         }
-        
-//         int i=0;
-//         char ch=max_char+'a';
-//         while(max_count>0 && i<n){
-//             s[i]=ch;
-//             i+=2;
-//             max_count--;
-//         }
-//         heap[max_char]=0;
-        
-//         if(max_count!=0){
-//             return "";
-//         }
-        
-//         for(int j=0;j<26;j++){
-//             while(heap[j]>0){
-//                 if(i>=n){
-//                     i=1;
-//                 }
-//                 s[i]=j+'a';
-//                 i+=2;
-//                 heap[j]--;
-//             }
-//         }
-        
-//         return s;
-        
-        
-        
-        //Using Priority Queue
-        
         int n=s.length();
         int heap[26]={0};
         priority_queue<pair<int, char>> que;
@@ -82,17 +30,6 @@ public:
             if(--prev.first)que.push(prev);
             prev=curr;
         }
-        // for(int j=0;j<26;j++){
-        //     while(heap[j]>0){
-        //         if(i>=n){
-        //             i=1;
-        //         }
-        //         s[i]=j+'a';
-        //         i+=2;
-        //         heap[j]--;
-        //     }
-        // }
-        
         return s;
     }
 };
