811d15b3c394399478896360eb8eea8a51a0f6ad
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-14 15:16:41 +0530

    Time: 37 ms (71.94%), Space: 19.5 MB (86.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,24 +1,67 @@
 class Solution {
 public:
+    
+    // we are using the map with pair of string and vector
+    // when we sort two string and check that they are anagaram or not 
+    // by using == but we are not checking
+    // if the two strings are not same then they form a new group
+    // we use map to store pair of sorted string with their vector of 
+    // string
+    
+    // T.C=O()   S.C.=O(N)
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> maps;
-        int n=strs.size();
-        for(int i=0;i<n;i++){
-            string temp=strs[i];
-            sort(temp.begin(),temp.end());
-            if(maps.find(temp) == maps.end()){
-                vector<string> inner;
-                inner.push_back(strs[i]);
-                maps[temp]=inner;
-            }
-            else{
-                maps[temp].push_back(strs[i]);
-            }
-        }
+//         unordered_map<string,vector<string>> maps;
+//         int n=strs.size();
+//         for(int i=0;i<n;i++){
+//             string temp=strs[i];
+//             sort(temp.begin(),temp.end());
+//             if(maps.find(temp) == maps.end()){
+//                 vector<string> inner;
+//                 inner.push_back(strs[i]);
+//                 maps[temp]=inner;
+//             }
+//             else{
+//                 maps[temp].push_back(strs[i]);
+//             }
+//         }
+        
+//         vector<vector<string>> ans;
+//         for(auto key:maps){
+//             ans.push_back(key.second);
+//         }
+//         return ans;
         
         vector<vector<string>> ans;
-        for(auto key:maps){
-            ans.push_back(key.second);
+        vector<pair<string,string>> sorted;
+        for(string s:strs){
+            string temp=s;
+            sort(temp.begin(),temp.end());
+            sorted.push_back({temp,s});
+        }
+        
+        int n=sorted.size();
+        vector<string> temp;
+        sort(sorted.begin(),sorted.end());
+        // for(int i=0;i<n;i++){
+        //     cout<<i+1<<" = "<<sorted[i].second<<" ";
+        // }
+        for(int i=0;i<n;i++){
+            if(i==0){
+                temp.push_back(sorted[i].second);
+            }
+            else if(sorted[i].first==sorted[i-1].first){
+                temp.push_back(sorted[i].second);
+            }
+            else{
+                ans.push_back(temp);
+                temp.clear();
+                temp.push_back(sorted[i].second);
+            }
+            // cout<<" count = "<<i<<endl;
+            
+        }
+        if(temp.size()>0){
+           ans.push_back(temp); 
         }
         return ans;
     }
