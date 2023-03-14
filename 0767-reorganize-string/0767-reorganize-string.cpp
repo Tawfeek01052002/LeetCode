111723d13381fcd134b6110af67a812578582565
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-14 14:13:42 +0530

    Time: 5 ms (28.06%), Space: 6.2 MB (88.87%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -5,47 +5,94 @@ public:
     // 2. finding max character with count
     // 3. put the characters with +2 diff
     // 4. checking if max_char is not fitting in the length
+    
+    //T.C.=O(n) S.C.=O(n)
     string reorganizeString(string s) {
-        int heap[26]={0};
+//         int heap[26]={0};
+//         int n=s.length();
+//         for(int i=0;i<n;i++){
+//             heap[s[i]-'a']++;
+//         }
+        
+//         int max_char=0;
+//         int max_count=INT_MIN;
+//         for(int i=0;i<26;i++){
+//             if(heap[i]>max_count){
+//                 max_count=heap[i];
+//                 max_char=i;
+//             }
+//         }
+        
+//         int i=0;
+//         char ch=max_char+'a';
+//         while(max_count>0 && i<n){
+//             s[i]=ch;
+//             i+=2;
+//             max_count--;
+//         }
+//         heap[max_char]=0;
+        
+//         if(max_count!=0){
+//             return "";
+//         }
+        
+//         for(int j=0;j<26;j++){
+//             while(heap[j]>0){
+//                 if(i>=n){
+//                     i=1;
+//                 }
+//                 s[i]=j+'a';
+//                 i+=2;
+//                 heap[j]--;
+//             }
+//         }
+        
+//         return s;
+        
+        
+        
+        //Using Priority Queue
+        
         int n=s.length();
+        int heap[26]={0};
+        priority_queue<pair<int, char>> que;
         for(int i=0;i<n;i++){
             heap[s[i]-'a']++;
+            if(heap[s[i]-'a']>(s.size()+1)/2){
+                return "";
+            }
         }
-        
-        int max_char=0;
-        int max_count=INT_MIN;
         for(int i=0;i<26;i++){
-            if(heap[i]>max_count){
-                max_count=heap[i];
-                max_char=i;
+            if(heap[i]){
+                que.push({heap[i],(i+'a')});
             }
         }
-        
+    
         int i=0;
-        char ch=max_char+'a';
-        while(max_count>0 && i<n){
-            s[i]=ch;
-            i+=2;
-            max_count--;
-        }
-        heap[max_char]=0;
+        pair<int,char> prev=que.top();
+        char ch=prev.second;
+        que.pop();
+        s[i++]=ch;
         
-        if(max_count!=0){
-            return "";
+        while(!que.empty()){
+            pair<int,char> curr=que.top();
+            char ch1=curr.second;
+            que.pop();
+            s[i++]=ch1;
+            if(--prev.first)que.push(prev);
+            prev=curr;
         }
-        
-        for(int j=0;j<26;j++){
-            while(heap[j]>0){
-                if(i>=n){
-                    i=1;
-                }
-                s[i]=j+'a';
-                i+=2;
-                heap[j]--;
-            }
-        }
+        // for(int j=0;j<26;j++){
+        //     while(heap[j]>0){
+        //         if(i>=n){
+        //             i=1;
+        //         }
+        //         s[i]=j+'a';
+        //         i+=2;
+        //         heap[j]--;
+        //     }
+        // }
         
         return s;
-        
     }
 };
