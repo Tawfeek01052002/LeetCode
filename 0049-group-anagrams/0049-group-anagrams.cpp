8909636dff5943563717279c2f360226025a1799
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-14 15:22:52 +0530

    Time: 28 ms (95.12%), Space: 20.8 MB (34.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -8,61 +8,58 @@ public:
     // we use map to store pair of sorted string with their vector of 
     // string
     
-    // T.C=O()   S.C.=O(N)
+    // T.C=O(n*mlogm)   S.C.=O(N)
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-//         unordered_map<string,vector<string>> maps;
-//         int n=strs.size();
-//         for(int i=0;i<n;i++){
-//             string temp=strs[i];
-//             sort(temp.begin(),temp.end());
-//             if(maps.find(temp) == maps.end()){
-//                 vector<string> inner;
-//                 inner.push_back(strs[i]);
-//                 maps[temp]=inner;
-//             }
-//             else{
-//                 maps[temp].push_back(strs[i]);
-//             }
-//         }
-        
-//         vector<vector<string>> ans;
-//         for(auto key:maps){
-//             ans.push_back(key.second);
-//         }
-//         return ans;
+        unordered_map<string,vector<string>> maps;
+        int n=strs.size();
+        for(int i=0;i<n;i++){
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            maps[temp].push_back(strs[i]);
+          
+        }
         
         vector<vector<string>> ans;
-        vector<pair<string,string>> sorted;
-        for(string s:strs){
-            string temp=s;
-            sort(temp.begin(),temp.end());
-            sorted.push_back({temp,s});
-        }
-        
-        int n=sorted.size();
-        vector<string> temp;
-        sort(sorted.begin(),sorted.end());
-        // for(int i=0;i<n;i++){
-        //     cout<<i+1<<" = "<<sorted[i].second<<" ";
-        // }
-        for(int i=0;i<n;i++){
-            if(i==0){
-                temp.push_back(sorted[i].second);
-            }
-            else if(sorted[i].first==sorted[i-1].first){
-                temp.push_back(sorted[i].second);
-            }
-            else{
-                ans.push_back(temp);
-                temp.clear();
-                temp.push_back(sorted[i].second);
-            }
-            // cout<<" count = "<<i<<endl;
-            
-        }
-        if(temp.size()>0){
-           ans.push_back(temp); 
+        for(auto key:maps){
+            ans.push_back(key.second);
         }
         return ans;
+        
+        
+        // T.C=O(n*klogk)  where n=length of vector k=length of maxi string
+        // S.C.=O(n)
+//         vector<vector<string>> ans;
+//         vector<pair<string,string>> sorted;
+//         for(string s:strs){
+//             string temp=s;
+//             sort(temp.begin(),temp.end());
+//             sorted.push_back({temp,s});
+//         }
+        
+//         int n=sorted.size();
+//         vector<string> temp;
+//         sort(sorted.begin(),sorted.end());
+//         // for(int i=0;i<n;i++){
+//         //     cout<<i+1<<" = "<<sorted[i].second<<" ";
+//         // }
+//         for(int i=0;i<n;i++){
+//             if(i==0){
+//                 temp.push_back(sorted[i].second);
+//             }
+//             else if(sorted[i].first==sorted[i-1].first){
+//                 temp.push_back(sorted[i].second);
+//             }
+//             else{
+//                 ans.push_back(temp);
+//                 temp.clear();
+//                 temp.push_back(sorted[i].second);
+//             }
+//             // cout<<" count = "<<i<<endl;
+            
+//         }
+//         if(temp.size()>0){
+//            ans.push_back(temp); 
+//         }
+//         return ans;
     }
 };
