0a25725dfa4d5c217571334db602b6405d2c136c
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-03-16 23:58:15 +0530

    Time: 11 ms (73.72%), Space: 6 MB (75.97%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mapped(char ch){
+        switch(ch){
+            case 'M':return 1000;
+            case 'D':return 500;
+            case 'C':return 100;
+            case 'L':return 50;
+            case 'X':return 10;
+            case 'V':return 5;
+            case 'I':return 1;
+        }
+        return -1;
+    }
+    int romanToInt(string s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(mapped(s[i])<mapped(s[i+1])){
+                ans+=(mapped(s[i+1])-mapped(s[i]));
+                i++;
+            }
+            else{
+                ans+=(mapped(s[i]));
+            }
+        }
+        return ans;
+    }
+};
