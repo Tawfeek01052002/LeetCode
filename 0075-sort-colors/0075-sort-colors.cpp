5e25652d090d08f5c8ae1b741c03efe26b3eef81
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-21 16:03:48 +0530

    Time: 3 ms (47.27%), Space: 8.2 MB (43.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,24 +7,24 @@ public:
         //  sort(nums.begin(),nums.end());
 
         // Using Counting
-        int zero = 0, one = 0, two = 0;
+//         int zero = 0, one = 0, two = 0;
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == 0)
-                zero++;
-            else if (nums[i] == 1)
-                one++;
-            else
-                two++;
-        }
-        int i = 0;
-        while (zero-- > 0)
-            nums[i++] = 0;
-        while (one-- > 0)
-            nums[i++] = 1;
-        while (two-- > 0)
-            nums[i++] = 2;
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             if (nums[i] == 0)
+//                 zero++;
+//             else if (nums[i] == 1)
+//                 one++;
+//             else
+//                 two++;
+//         }
+//         int i = 0;
+//         while (zero-- > 0)
+//             nums[i++] = 0;
+//         while (one-- > 0)
+//             nums[i++] = 1;
+//         while (two-- > 0)
+//             nums[i++] = 2;
 
         // Dutch National Flag
         // Using 3 Pointers
