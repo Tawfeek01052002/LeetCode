6cd015a7bb961da580a1e0909512f1f71c2dca31
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-21 16:03:41 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (9.51%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,40 +1,42 @@
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
-        
-        //Using Sort function
-        // sort(nums.begin(),nums.end());
-        
-        //Using Counting
-//         int zero=0,one=0,two=0;
-        
-//         for(int i=0;i<nums.size();i++){
-//             if(nums[i]==0)
-//                 zero++;
-//             else if(nums[i]==1)
-//                 one++;
-//             else
-//                 two++;
-//         }
-//         int i=0;
-//         while(zero-->0)
-//             nums[i++]=0;
-//         while(one-->0)
-//             nums[i++]=1;
-//         while(two-->0)
-//             nums[i++]=2;
-        
-        //Dutch National Flag 
-        //Using 3 Pointers
-        int low=0,mid=0,high=nums.size()-1;
-        while(mid<=high){
-            if(nums[mid]==0)
-                swap(nums[mid++],nums[low++]);
-            else if(nums[mid]==2)
-                swap(nums[mid],nums[high--]);
+    void sortColors(vector<int> &nums)
+    {
+        // Using Sort function
+        //  sort(nums.begin(),nums.end());
+
+        // Using Counting
+        int zero = 0, one = 0, two = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+                zero++;
+            else if (nums[i] == 1)
+                one++;
+            else
+                two++;
+        }
+        int i = 0;
+        while (zero-- > 0)
+            nums[i++] = 0;
+        while (one-- > 0)
+            nums[i++] = 1;
+        while (two-- > 0)
+            nums[i++] = 2;
+
+        // Dutch National Flag
+        // Using 3 Pointers
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+                swap(nums[mid++], nums[low++]);
+            else if (nums[mid] == 2)
+                swap(nums[mid], nums[high--]);
             else
                 mid++;
         }
-        
     }
 };
