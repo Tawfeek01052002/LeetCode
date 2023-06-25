78ea6a080826610e9b96b66bafbad6426282b0ca
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-26 03:59:05 +0530

    Time: 20 ms (97.87%), Space: 22.3 MB (81.05%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,20 +4,24 @@ public:
     int start = 0;
     int end = arr.size() - 1;
     int mid = 0;
-    while (start < end)
+    while (start <= end)
     {
         mid = start + (end - start) / 2;
+        if(start==end)
+            return arr[mid];
         if((mid & 1) == 0) {
             if (mid + 1 <= arr.size() - 1 && arr[mid] == arr[mid + 1])
                 start = mid + 2; 
             else
                 end=mid;
         }
-        else if (mid - 1 >= 0 && arr[mid] == arr[mid - 1])
+        else{
+            if (mid - 1 >= 0 && arr[mid] == arr[mid - 1])
                 start = mid + 1;
-        else
+            else
                 end = mid-1;
+            }
         }
-    return arr[start];
+    return -1;
     }
 };
