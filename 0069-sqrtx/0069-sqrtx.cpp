ef9138f34e5f3c9d9094d6eaaeb59a10a11c571e
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-25 19:21:57 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (94.05%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
-        //using inbuilt function
-        // return sqrt(x);
-        
         //using binary search
-        int s=1;
-        int e=x;
-        int mid=s+(e-s)/2;
-        while(s<=e){
+        int start=1,mid;
+        int end=x;
+        int num=0;
+        while(start<=end){
+            mid=start+(end-start)/2;
             if(mid==x/mid){
                 return mid;
             }
-            else if(mid>x/mid){
-                e=mid-1;
+            else if(mid<x/mid){
+                num=mid;
+                start=mid+1;
             }
-            else{
-                s=mid+1;
-            }
-            mid=s+(e-s)/2;
+            else
+                end=mid-1;
         }
-        return e;
-        
+        return num;
     }
 };
