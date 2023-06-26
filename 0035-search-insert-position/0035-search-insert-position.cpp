84cbb3636c84189bdea442dc7db17787b179afc0
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-27 00:32:16 +0530

    Time: 5 ms (62.10%), Space: 9.8 MB (13.43%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     int searchInsert(vector<int>& arr, int target) {
-        int start=0;
-        int end=arr.size()-1;
-        int index=end+1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]>target){
-                index=mid;
-                end=mid-1;
-            }
-            else
-                start=mid+1;
+        // int start=0;
+        // int end=arr.size()-1;
+        // int index=end+1;
+        // while(start<=end){
+        //     int mid=start+(end-start)/2;
+        //     if(arr[mid]==target)
+        //         return mid;
+        //     else if(arr[mid]>target){
+        //         index=mid;
+        //         end=mid-1;
+        //     }
+        //     else
+        //         start=mid+1;
+        // }
+        // return index;
+        int left = 0;
+    int right = arr.size() - 1;
+    int ceil = arr.size();
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+        {
+            ceil = mid;
+            right = mid - 1;
         }
-        return index;
+    }
+
+    return ceil;
     }
 };
