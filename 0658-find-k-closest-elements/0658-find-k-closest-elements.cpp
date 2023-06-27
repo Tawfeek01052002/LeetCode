5618758f1ea904556006cf46a511216bdd4a1c74
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-27 20:14:46 +0530

    Time: 40 ms (90.95%), Space: 31 MB (98.48%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-    int binarySearch(vector<int> arr,int x){
-        int start=0;
-        int end=arr.size()-1;
-        int index=arr.size();
-        while(start<end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]==x)
-                return mid;
-            else if(arr[mid]<x)
-                start=mid+1;
-            else
-                end=mid;
-        }
-        return end;
-    }
-    
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
-        
-        //Using 2 pointers  
-        // T.C.=O(n-k) S.C.=O(1)
-//         int n=arr.size();
-//         int start=0;
-//         int end=n-1;
-        
-//         while(end-start>=k){
-//             if(abs(arr[start]-x)>abs(arr[end]-x)){
-//                 start++;
-//             }
-//             else end--;
-//         }
-//         return vector<int>(arr.begin()+start, arr.begin()+end+1);
-        
-        
-        //Slightly better approach same as previous using 2 pointers
-        // step1: find smallest element which is greter than x
-        // after that expand the start and end after comparing
+
+        int s=0;
+        int e=arr.size()-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]==x){
+                e=mid;
+                break;
+            }
+            else if(arr[mid]<x)
+                s=mid+1;
+            else
+                e=mid;
+        }        
         int n=arr.size();
-        int up=binarySearch(arr,x);
-        int start=up-1;
-        int end=up;
+        int start=e-1;
+        int end=e;
         cout<<"Start = "<<start<<endl;
         cout<<"End = "<<end<<endl;
         while(start>=0 && end<n && k>0){
