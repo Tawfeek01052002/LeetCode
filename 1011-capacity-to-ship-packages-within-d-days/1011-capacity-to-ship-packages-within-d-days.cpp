ecdf2059aec0af76c48da3789315ade40e26174e
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-29 20:08:13 +0530

    Time: 109 ms (20.25%), Space: 31 MB (60.71%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -26,20 +26,17 @@ public:
     
     //Using Book Allocation Problem
     int shipWithinDays(vector<int>& weights, int days) {
-        int s=0;
-        int e=0;
-        for(int i=0;i<weights.size();i++){
-            e+=weights[i];
-        }
+        int start=0;
+        int end=accumulate(weights.begin(),weights.end(),0);
         int ans=-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
             if(isPossibleSolution(weights,days,mid)){
                 ans=mid;
-                e=mid-1;
+                end=mid-1;
             }
             else{
-                s=mid+1;
+                start=mid+1;
             }
         }
         return ans;
