e09ec8f511c4266494e1700a6d543ab6b3f7c6ef
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-06-30 22:14:58 +0530

    Time: 11 ms (96.74%), Space: 10.9 MB (67.47%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -5,9 +5,7 @@ public:
         int n=s.length();
         string ans="";
         for(int i=0;i<n;i++){
-            if(ans.size()==0)
-                ans.push_back(s[i]);
-            else if(ans[ans.size()-1]==s[i])
+            if(ans.size()>0 && ans[ans.size()-1]==s[i])
                 ans.pop_back();
             else
                 ans.push_back(s[i]);
