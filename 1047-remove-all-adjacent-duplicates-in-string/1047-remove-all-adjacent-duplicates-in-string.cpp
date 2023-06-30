1ee3f9b3d70ce64b6876f582a66fff88594715ad
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-01 00:41:52 +0530

    Time: 13 ms (90.27%), Space: 10.4 MB (87.38%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,15 +2,31 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         
-        int n=s.length();
-        string ans="";
-        for(int i=0;i<n;i++){
-            if(ans.size()>0 && ans[ans.size()-1]==s[i])
-                ans.pop_back();
-            else
-                ans.push_back(s[i]);
+        
+        // T.C.=O(n)  S.C.=O(n)
+        // int n=s.length();
+        // string ans="";
+        // for(int i=0;i<n;i++){
+        //     if(ans.size()>0 && ans[ans.size()-1]==s[i])
+        //         ans.pop_back();
+        //     else
+        //         ans.push_back(s[i]);
+        // }
+        // return ans;
+        
+        // T.C.=O(n)  S.C.=O(1)
+        int i=0;
+        int j=1;
+        while(j<s.length()){
+            if(i>=0 && s[i]==s[j]){
+                i--;
+                j++;
+            }
+            else{
+                s[++i]=s[j++];
+            }
         }
-        return ans;
+        return s.substr(0,i+1);
         
     }
 };
