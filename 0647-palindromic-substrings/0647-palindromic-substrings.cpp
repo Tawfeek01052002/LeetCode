19a6244b2b37c917f2efafbaf421d0c80cfeb35f
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-01 14:13:44 +0530

    Time: 9 ms (60.89%), Space: 13.1 MB (42.34%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,67 +1,36 @@
 class Solution {
 public:
-    // bool isPalindrome(string str, int s, int e)
-    // {
-    //     while (s < e)
-    //     {
-    //         if (str[s] != str[e])
-    //             return false;
-    //         s++;
-    //         e--;
-    //     }
-    //     return true;
-    // }
     
-    // Brute force approach checking all possible substring is palindrome       or not 
-    // int countSubstrings(string str1)
-    // {
-    //     int n = str1.length();
-    //     int count = 0;
-    //     for (int i = 0; i < n; i++)
-    //     {
-    //         for (int j = i; j < n; j++)
-    //         {
-    //             if (isPalindrome(str1, i, j))
-    //             {
-    //                 count++;
-    //             }
-    //             // cout << str1.substr(i, j) << endl;
-    //         }
-    //     }
-    //     return count;
-    // }
-    
-    //we are check that substring(i,j) is palindrome or not if yes
-    // then i-- and j++ again check with this
-    // two cases comes of even length substring and odd length substring
-    int isPalindrome(string str,int i,int j){
+    //substrings of odd and even length
+    int numberOfOddPalindrome(string s,int i){
+        int j=i;
+        int n=s.length();
         int count=0;
-        int n=str.length();
-        while(i>=0 && j<n){
-            if(str[i]==str[j]){
+        while(i>=0 && j<n && s[i]==s[j]){
+            count++;
+            j++;
+            i--;
+        }
+        return count;
+    }
+    
+    int numberOfEvenPalindrome(string s,int i){
+        int j=i+1;
+        int n=s.length();
+        int count=0;
+        while(i>=0 && j<n && s[i]==s[j]){
                 count++;
-                i--;
                 j++;
-            }
-            else{
-                return count;
-            }
+                i--;
         }
         return count;
     }
-    
-    //Optimised Approach
-    
-    int countSubstrings(string str1)
-    {
-        int n = str1.length();
-        int count = 0;
-        for (int i = 0; i < n ; i++)
-        {
-            count+=isPalindrome(str1,i,i);
-            count+=isPalindrome(str1,i,i+1);
+    int countSubstrings(string s) {
+        int n=s.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            count=count+numberOfEvenPalindrome(s,i)+numberOfOddPalindrome(s,i);
         }
         return count;
     }
-    
 };
