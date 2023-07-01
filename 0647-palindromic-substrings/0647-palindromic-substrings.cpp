3322179acca07dcc19cf5e0deb3c647d7a439071
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-01 14:16:17 +0530

    Time: 7 ms (71.27%), Space: 13.3 MB (33.43%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     
     //substrings of odd and even length
-    int numberOfOddPalindrome(string s,int i){
-        int j=i;
+    int numberOfPalindrome(string s,int i,int j){
         int n=s.length();
         int count=0;
         while(i>=0 && j<n && s[i]==s[j]){
@@ -13,23 +12,11 @@ public:
         }
         return count;
     }
-    
-    int numberOfEvenPalindrome(string s,int i){
-        int j=i+1;
-        int n=s.length();
-        int count=0;
-        while(i>=0 && j<n && s[i]==s[j]){
-                count++;
-                j++;
-                i--;
-        }
-        return count;
-    }
     int countSubstrings(string s) {
         int n=s.length();
         int count=0;
         for(int i=0;i<n;i++){
-            count=count+numberOfEvenPalindrome(s,i)+numberOfOddPalindrome(s,i);
+            count=count+numberOfPalindrome(s,i,i)+numberOfPalindrome(s,i,i+1);
         }
         return count;
     }
