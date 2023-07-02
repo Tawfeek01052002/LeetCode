33fa1f36a47d0b6b77272deeea94f9527c993247
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-02 21:19:59 +0530

    Time: 3 ms (98.33%), Space: 7.3 MB (41.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    // T.C.=O(N+M)  S.C.==O(1)
-    //Good Solution we can also use map but avoid use of map it will take aroun O(N) space
     bool isAnagram(string s, string t) {
         int arr[256]={0};
-        int sn=s.length();
-        int tn=t.length();
-        for(int i=0;i<sn;i++){
-            // cout<<(int)s[i]<<endl;
-            arr[(int)s[i]]+=1;
-            // cout<<arr[(int)s[i]]<<endl;
+        for(int i=0;i<s.length();i++){
+            arr[s[i]]++;
         }
-        for(int i=0;i<tn;i++){
-            arr[(int)t[i]]--;
-        }
+        for(int i=0;i<t.length();i++){
+            arr[t[i]]--;
+        }   
         for(int i=0;i<256;i++){
-            if(arr[i]!=0){
+            if(arr[i]!=0)
                 return false;
-            }
         }
         return true;
-        
-        
-        
-        //Method 2: Not Better Solution
-        //Using sorting T.C.=O(nlogn) S.C.=O(1)
-        // sort(s.begin(),s.end());
-        // sort(t.begin(),t.end());
-        // return s==t?true:false;
-        
     }
 };
