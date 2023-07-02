4b8879c71f4f3daedf4878736202d3d6ec85f069
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-02 21:43:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (56.35%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.cpp
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
-    // T.C.=O(N)  S.C=O(1)
-//     string reverseOnlyLetters(string str) {
-//         int s=0;
-//         int e=str.length()-1;
-//         while(s<e){
-//             if(str[s]>='A' && str[s]<='Z' || str[s]>='a' && str[s]<='z'){
-//                 if(str[e]>='A' && str[e]<='Z' || str[e]>='a' && str[e]<='z')
-//                     swap(str[s++],str[e--]);
-//                 else
-//                     e--;
-//             }
-//             else{
-//                 s++;
-//             }
-//         }
-//         return str;
-//     }
-    
-    
-    //Same code but written simply
-    string reverseOnlyLetters(string str) {
-        int s=0;
-        int e=str.length()-1;
-        while(s<e){
-            if(isalpha(str[s])){
-                if(isalpha(str[e]))
-                    swap(str[s++],str[e--]);
-                else
-                    e--;
-            }
-            else{
-                s++;
-            }
+    string reverseOnlyLetters(string s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<=end){
+            if(!isalpha(s[start]))
+                start++;
+            else if(!isalpha(s[end]))
+                end--;
+            else
+                swap(s[start++],s[end--]);
         }
-        return str;
+        return s;
     }
-    
-    
 };
