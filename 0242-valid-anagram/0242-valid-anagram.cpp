8a5d48cacefc478de892ab8780c30257921f7770
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-02 21:20:25 +0530

    Time: 4 ms (95.77%), Space: 7.3 MB (80.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         int arr[256]={0};
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++)
             arr[s[i]]++;
-        }
-        for(int i=0;i<t.length();i++){
+        for(int i=0;i<t.length();i++)
             arr[t[i]]--;
-        }   
         for(int i=0;i<256;i++){
             if(arr[i]!=0)
                 return false;
