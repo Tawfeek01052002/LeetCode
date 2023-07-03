77f336e4d92d597db416b216eb42f87177f54a45
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-04 03:25:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (96.40%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -7,16 +7,22 @@ public:
         int n=s.length();
         int arr[26]={0};
         char maxOcc=s[0];
+        
+        //adding frequency of character on array with size 26
+        // and during this we are finding maxOccured character
         for(int i=0;i<n;i++){
             arr[s[i]-'a']++;
             if(arr[s[i]-'a']>arr[maxOcc-'a'])
                 maxOcc=s[i];
         }
+        
+        //checking not possible condition
         if(arr[maxOcc-'a']>(n+1)/2)
             return "";
         
         string ans(n,' ');
         
+        //filling the output string on difference of i+2
         int i=0;
         while(arr[maxOcc-'a']!=0){
                 ans[i]=maxOcc;
