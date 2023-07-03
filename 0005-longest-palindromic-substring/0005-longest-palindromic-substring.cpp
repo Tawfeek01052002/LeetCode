44c019c09a8aeca36391299aff6c784d30aee800
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-04 03:49:19 +0530

    Time: 100 ms (54.12%), Space: 217.8 MB (17.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     string checkPalindrome(string s,int i,int j,string ans){
+        //     bool isPalindrome(string &str,int i,int j){
+        //         while(i<j){
+        //             if(str[i]!=str[j]){
+        //                 return false;
+        //             }
+        //             i++;
+        //             j--;
+        //         }
+        //         return true;
+        //     }
+        
+        //Brute Force Approach 
+        // T.C.=O(N^3)  S.C.=O(1)
+        // string ans="";
+        // int n=s.length();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         if(isPalindrome(s,i,j)){
+        //             string t=s.substr(i,j-i+1);
+        //             ans=t.size()>ans.size()?t:ans;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        // T.C.=O(N^2) S.C.=O(1)
+        //Good Method
         int max=ans.length();
         while(i>=0 && j<s.length() && s[i]==s[j]){
             if(j-i>=max)
