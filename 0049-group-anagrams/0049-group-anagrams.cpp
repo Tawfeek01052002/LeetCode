1481467ecb2b6c3ef44a07927ce3979496906d7b
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-04 17:15:43 +0530

    Time: 145 ms (8.63%), Space: 41.9 MB (5.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     
+    // we are using the map with pair of string and vector
+    // when we sort two string and check that they are anagaram or not 
+    // by using == but we are not checking
+    // if the two strings are not same then they form a new group
+    // we use map to store pair of sorted string with their vector of 
+    // string
+    
+    
     std::array<int,256> getHash(string s){
         std::array<int,256> arr={0};
         for(int i=0;i<s.length();i++){
@@ -9,8 +17,64 @@ public:
         return arr;
     }
     
+    // T.C=O(n*mlogm)   S.C.=O(n*m)
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //using map 
+//         unordered_map<string,vector<string>> maps;
+//         int n=strs.size();
+//         for(int i=0;i<n;i++){
+//             string temp=strs[i];
+//             sort(temp.begin(),temp.end());
+//             maps[temp].push_back(strs[i]);
+          
+//         }
+        
+//         vector<vector<string>> ans;
+//         for(auto key:maps){
+//             ans.push_back(key.second);
+//         }
+//         return ans;
+        
+        
+        // T.C=O(n*klogk)  where n=length of vector k=length of maxi string
+        // S.C.=O(n)
+//         vector<vector<string>> ans;
+//         vector<pair<string,string>> sorted;
+//         for(string s:strs){
+//             string temp=s;
+//             sort(temp.begin(),temp.end());
+//             sorted.push_back({temp,s});
+//         }
+        
+//         int n=sorted.size();
+//         vector<string> temp;
+//         sort(sorted.begin(),sorted.end());
+//         // for(int i=0;i<n;i++){
+//         //     cout<<i+1<<" = "<<sorted[i].second<<" ";
+//         // }
+//         for(int i=0;i<n;i++){
+//             if(i==0){
+//                 temp.push_back(sorted[i].second);
+//             }
+//             else if(sorted[i].first==sorted[i-1].first){
+//                 temp.push_back(sorted[i].second);
+//             }
+//             else{
+//                 ans.push_back(temp);
+//                 temp.clear();
+//                 temp.push_back(sorted[i].second);
+//             }
+//             // cout<<" count = "<<i<<endl;
+            
+//         }
+//         if(temp.size()>0){
+//            ans.push_back(temp); 
+//         }
+//         return ans;
+        
+        
+        
+        // Optimised Solution Using array of 256 size to check that anagram or not
+        // T.C=O(n*k)   S.C.=O(n*k)+map_size
         map<std::array<int,256>,vector<string>> mp;
         for(auto ele:strs){
             mp[getHash(ele)].push_back(ele);
@@ -21,5 +85,7 @@ public:
             ans.push_back(ele.second);
         }
         return ans;
+        
+        
     }
 };
