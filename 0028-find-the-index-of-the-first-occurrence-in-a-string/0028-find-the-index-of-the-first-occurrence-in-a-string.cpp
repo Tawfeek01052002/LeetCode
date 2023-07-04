99ddb3bfb33f3f6ad1fb0d38cc3eb15f45377de8
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-04 17:51:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (58.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -10,11 +10,27 @@ public:
         
         //checking substrings
         // also called we are using sliding window
+        // int n=haystack.length();
+        // int m=needle.length();
+        // for(int i=0;i<=n-m;i++){
+        //     if(haystack[i]==needle[0] && haystack.substr(i,m).compare(needle)==0)
+        //         return i;
+        // }
+        // return -1;
+        
+        
+        //without using substr
         int n=haystack.length();
         int m=needle.length();
-        for(int i=0;i<=n-m;i++){
-            if(haystack[i]==needle[0] && haystack.substr(i,m).compare(needle)==0)
+        for(int i=0,j;i<=n-m;i++){
+            for(j=0;j<m;j++){
+                if(needle[j]!=haystack[i+j]){
+                    break;
+                }
+            }
+            if(j==m){
                 return i;
+            }
         }
         return -1;
         
