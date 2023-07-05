380c4b3484dbb1c7a428881ba943146d47447d4d
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-05 17:06:09 +0530

    Time: 8 ms (80.20%), Space: 10.7 MB (37.85%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    
+    //we are using vectors to store character 
+    //like we are traversing throw vector  
+    //like placing charcter if numRows=3 then on 0, 1, 2, 
+    //then 1, 0  then 1, 2 likewise 
+    
+    
     string convert(string s, int numRows) {
         if(numRows==1 ||s.length()==1){
             return s;
