cc7cf9a98a5e914f424a230bcf17bc06eb6857d0
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-05 14:07:14 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (18.73%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        // 1.Ignore leading whitespaces
-        // 2.determine sign
-        // 3.if digit found after finding sign
-        // 4.fill till next non-digit char is found
-        // 5.out of range handling
-        
-        int i=0,sign=1,num=0;
-        while(s[i]==' '){
+        int i=0;
+        int ans=0;
+        int sign=1;
+        while(i<s.length() && s[i]==' '){
             i++;
         }
-        bool b=false;
-        while(i<s.size() && (s[i]=='-' || s[i]=='+')){
-            if(b){
-                return 0;
-            }
-            sign= s[i]=='+'?1:-1;
+        
+        if(i<s.length() && s[i]=='-' || s[i]=='+'){
+            sign= s[i]=='-'?-1:1;
             i++;
-            b=true;
-            
         }
         
-        while(i<s.size() && isdigit(s[i])){
-            if(num> INT_MAX/10 || (num== INT_MAX/10 && s[i]>'7'))
+        while(i<s.length() && isdigit(s[i])){ 
+            if(ans>INT_MAX/10 || (ans==INT_MAX/10 && s[i]>'7'))
                 return sign==-1?INT_MIN:INT_MAX;
-            num=num*10+(s[i]-'0');
+            ans=ans*10+(s[i]-'0');
             i++;
         }
-        return num*sign;
+        return ans*sign;
     }
 };
