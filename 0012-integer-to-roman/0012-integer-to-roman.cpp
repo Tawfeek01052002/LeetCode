44ebbf0572b1f0ecfb878c985d58778164e3b1c5
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-05 14:40:23 +0530

    Time: 3 ms (94.91%), Space: 5.9 MB (72.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    
+    //we are handling for this case
+    // I can be placed before V (5) and X (10) to make 4 and 9. 
+    // X can be placed before L (50) and C (100) to make 40 and 90. 
+    // C can be placed before D (500) and M (1000) to make 400 and 900.
+    // T.C.=O(n) S.C.=O(1)
     string intToRoman(int num) {
         string ans="";
         string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
