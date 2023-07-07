2a6946025860c7b6d4b1f4912c632714d4a3d742
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-07 14:53:48 +0530

    Time: 296 ms (37.31%), Space: 10.2 MB (54.91%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
+    bool isPrime(int n){
+        for(int i=2;i<=sqrt(n);i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
     int countPrimes(int n) {
-        if(n<=1)
+        
+        //T.C.=O(n √n) S.C.=O(1) //TLE
+        // if(n<=1)
+        //     return 0;
+        // int count=0;
+        // for(int i=2;i<n;i++){
+        //     if(isPrime(i))
+        //         count++;
+        // }
+        // return count;
+        
+        //sieve of eratosthenes
+        
+        if(n<2)
             return 0;
-        vector<bool> prime(n,true);
+        vector<bool> arr(n,true);
+        arr[0]=false;
+        arr[1]=false;
         int count=0;
-        prime[0]=prime[1]=false;
         for(int i=2;i<n;i++){
-            if(prime[i]){
-                count++;
-                int j=i*2;
-                while(j<n){
-                    prime[j]=false;
-                    j+=i;
+            if(arr[i]){
+                count++;   
+                int j=2;
+                while((i*j)<n){
+                    arr[i*j]=false; 
+                    j++;
                 }
             }
         }
