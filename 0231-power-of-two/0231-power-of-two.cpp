28f23b2d2546f01a2747f4d80795a4ed94903bbc
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-09 19:08:50 +0530

    Time: 3 ms (46.87%), Space: 5.9 MB (42.15%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n>0 && (n==2 || n==1))
-            return true;
-        if(n&1==1 || n==0)
-            return false;
-        return isPowerOfTwo(n/2);
+        // if(n>0 && (n==2 || n==1))
+        //     return true;
+        // if(n&1==1 || n==0)
+        //     return false;
+        // return isPowerOfTwo(n/2);
+        
+        if(n==0) return false;
+        return (n==1) ||(!(n&1) && isPowerOfTwo(n>>1));
     }
 };
