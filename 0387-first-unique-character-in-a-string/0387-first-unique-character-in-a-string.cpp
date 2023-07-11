fafa3f8cff4b66ebc7e06887044bba7dfadc1586
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-12 03:23:44 +0530

    Time: 19 ms (93.40%), Space: 10.5 MB (88.40%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -3,20 +3,32 @@ public:
     int firstUniqChar(string s) {
         //worst case
         //T.C.=O(n) S.C.=O(1)
-        int n=s.length();
-        if(n==1)
-            return 0;
-        for(int i=0;i<n;i++){
-            bool flag=false;
-            for(int j=0;j<n;j++){
-                if(i!=j && s[i]==s[j]){
-                    flag=true;
-                    break;
-                }
-            }
-            if(!flag){
+        // int n=s.length();
+        // if(n==1)
+        //     return 0;
+        // for(int i=0;i<n;i++){
+        //     bool flag=false;
+        //     for(int j=0;j<n;j++){
+        //         if(i!=j && s[i]==s[j]){
+        //             flag=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!flag){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        
+        //Good Solution
+        //T.C.=O(n) S.C.=O(1)
+        int arr[26]={0};
+        for(int i=0;i<s.length();i++){
+            arr[s[i]-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(arr[s[i]-'a']==1)
                 return i;
-            }
         }
         return -1;
     }
