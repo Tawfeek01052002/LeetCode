5d1f2486ffc25fae209cda31b986ff00b24ae19f
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-12 03:40:46 +0530

    Time: 16 ms (35.67%), Space: 8.4 MB (48.25%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool cmp(pair<char,int> a,pair<char,int> b){
+        return a.second>b.second;
+    }
+    string frequencySort(string s) {
+        vector<pair<char,int>> arr(256,{' ',0});
+        
+        for(int i=0;i<s.length();i++){
+            arr[s[i]].second+=1;
+            arr[s[i]].first=s[i];
+        }
+        
+        sort(arr.begin(),arr.end(),cmp);
+        
+        // for(int i=0;i<256;i++){
+        //     cout<< arr[i].second<<"== "<<arr[i].first<<endl;
+        // }
+        
+        string str;
+        for(int i=0;i<arr.size();i++){
+            while(arr[i].second-->0){
+                str.push_back(arr[i].first);
+            }
+        }
+        return str;
+    }
+};
