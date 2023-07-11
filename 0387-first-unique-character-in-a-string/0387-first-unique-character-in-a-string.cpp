a599a2490d20f8ab200c0c429781a07c6eb60e79
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-12 03:24:30 +0530

    Time: 18 ms (94.59%), Space: 10.5 MB (99.20%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -23,10 +23,11 @@ public:
         //Good Solution
         //T.C.=O(n) S.C.=O(1)
         int arr[26]={0};
-        for(int i=0;i<s.length();i++){
+        int n=s.length();
+        for(int i=0;i<n;i++){
             arr[s[i]-'a']++;
         }
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<n;i++){
             if(arr[s[i]-'a']==1)
                 return i;
         }
