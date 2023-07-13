bb15849a5ebcedd791fe4c4ca0ea1c8fcc858e94
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-13 18:14:11 +0530

    Time: 4 ms (61.15%), Space: 8.4 MB (31.85%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,37 +1,64 @@
 class Solution {
 public:
-    // string getSumString(string num1,int n1,string num2,int n2,string ans,int carry){
-    //     if()
-    // }
+    string getSumString(string& num1,int n1,string& num2,int n2,int carry){
+        
+        //base case
+        if(n1<0 && n2<0){
+            if(carry>0){
+                string ans;
+                ans.push_back(carry+'0');
+                return ans;
+            }
+            return "";
+        }
+            
+        //ek case
+        int p1=(n1>=0?num1[n1]:'0')-'0';
+        int p2=(n2>=0?num2[n2]:'0')-'0';
+        int sum=p1+p2+carry;
+        int digit=sum%10;
+        carry=sum/10;
+        string ans= getSumString(num1,n1-1,num2,n2-1,carry);
+        ans.push_back(digit+'0');
+        return ans;
+        
+    }
     string addStrings(string num1, string num2) {
         
-        int n1=num1.length();
-        int n2=num2.length();
-        string ans;
-        int carry=0;
-        while(n1>0 && n2>0){
-            int digit=num1[n1-1]-'0'+num2[n2-1]-'0'+carry;
-            ans.push_back((digit%10)+'0');
-            carry=digit/10;
-            n1--;
-            n2--;
-        }
-        while(n1>0){
-            int digit=num1[n1-1]-'0'+carry;
-            ans.push_back((digit%10)+'0');
-            carry=digit/10;  
-            n1--;
-        }
-        while(n2>0){
-            int digit=num2[n2-1]-'0'+carry;
-            ans.push_back((digit%10)+'0');
-            carry=digit/10;
-            n2--;
-        }
-        if(carry>0){
-            ans.push_back(carry+'0');
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        //T.C.=O(N) S.C.=O(n)
+        // int n1=num1.length();
+        // int n2=num2.length();
+        // string ans;
+        // int carry=0;
+        // while(n1>0 && n2>0){
+        //     int digit=num1[n1-1]-'0'+num2[n2-1]-'0'+carry;
+        //     ans.push_back((digit%10)+'0');
+        //     carry=digit/10;
+        //     n1--;
+        //     n2--;
+        // }
+        // while(n1>0){
+        //     int digit=num1[n1-1]-'0'+carry;
+        //     ans.push_back((digit%10)+'0');
+        //     carry=digit/10;  
+        //     n1--;
+        // }
+        // while(n2>0){
+        //     int digit=num2[n2-1]-'0'+carry;
+        //     ans.push_back((digit%10)+'0');
+        //     carry=digit/10;
+        //     n2--;
+        // }
+        // if(carry>0){
+        //     ans.push_back(carry+'0');
+        // }
+        // reverse(ans.begin(),ans.end());
+        // return ans;
+        
+        //using recursion
+        int n1=num1.length()-1;
+        int n2=num2.length()-1;
+        
+        return getSumString(num1,n1,num2,n2,0);
     }
 };
