db97ad774ae928763e7993f9bfdbcb6a329f55f8
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-13 18:58:46 +0530

    Time: 6 ms (30.94%), Space: 6.5 MB (85.31%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        int index=s.find(part);
-        while(index!=string::npos){
-            s.erase(index,part.length());
-            index=s.find(part);
+    void removeOcc(string& s,string& t){
+        int index=s.find(t);
+        if(index==string::npos){
+            return;
         }
+        else{
+            s.erase(index,t.length());
+        }
+        
+        removeOcc(s,t);
+    }
+    string removeOccurrences(string& s, string& part) {
+        // int index=s.find(part);
+        // while(index!=string::npos){
+        //     s.erase(index,part.length());
+        //     index=s.find(part);
+        // }
+        // return s;
+        removeOcc(s,part);
         return s;
     }
 };
