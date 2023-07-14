7622ec4eb707d9635ecd492c711d676a67cd731c
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-14 16:39:25 +0530

    Time: 144 ms (68.16%), Space: 101.2 MB (6.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
+    
+    void maxPro(vector<int>& arr,int& diff,int& mini,int i){
+        if(i>=arr.size()){
+            return;
+        }
+        if(arr[i]<mini)
+            mini=arr[i];
+        else
+            diff=max(diff,arr[i]-mini);
+        
+        maxPro(arr,diff,mini,i+1);
+    }
+    
     int maxProfit(vector<int>& prices) { 
         
         //T.C.=O(n) S.C.=O(1)
+        // int diff=0;
+        // int minSofar=prices[0];
+        // for(int i=1;i<prices.size();i++){
+        //     if(prices[i]<minSofar)
+        //         minSofar=prices[i];
+        //     else diff=max(diff,prices[i]-minSofar);
+        // }
+        // return diff;
+        
+        //using recursion
         int diff=0;
-        int minSofar=prices[0];
-        for(int i=1;i<prices.size();i++){
-            if(prices[i]<minSofar)
-                minSofar=prices[i];
-            else diff=max(diff,prices[i]-minSofar);
-        }
+        int mini=prices[0];
+        int i=0;
+        maxPro(prices,diff,mini,i);
         return diff;
     }
 };
