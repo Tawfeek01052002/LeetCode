2f6f4a9f925e41b11d256fdbc6f242692aed56d3
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-14 16:48:21 +0530

    Time: 3 ms (75.51%), Space: 7.2 MB (13.74%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    void removeOcc(string& s,string& t){
-        int index=s.find(t);
-        if(index==string::npos){
-            return;
-        }
-        else{
-            // s.erase(index,t.length());
-            //this erase function can also be done as
-            string left=s.substr(0,index);
-            string right=s.substr(index+t.size(),s.size());
-            s=left+right;
-            removeOcc(s,t);
-        }
+//     void removeOcc(string& s,string& t){
+//         int index=s.find(t);
+//         if(index==string::npos){
+//             return;
+//         }
+//         else{
+//             // s.erase(index,t.length());
+//             //this erase function can also be done as
+//             string left=s.substr(0,index);
+//             string right=s.substr(index+t.size(),s.size());
+//             s=left+right;
+//             removeOcc(s,t);
+//         }
         
+//     }
+    
+    string removeOcc(string& s,string& t){
+        int index = s.find(t);
+        if (index != string::npos)
+        {
+            s.erase(index, t.length());
+            removeOcc(s, t);
+        }
+        return s;
     }
+    
     string removeOccurrences(string& s, string& part) {
         // int index=s.find(part);
         // while(index!=string::npos){
@@ -37,7 +48,11 @@ public:
         //O(N/M * N*M)==O(N^2)
         
         //T.C.=O(N^2) S.C.=O(N/M)
-        removeOcc(s,part);
-        return s;
+        // removeOcc(s,part);
+        // return s;
+        
+        return removeOcc(s,part);
+        
+        
     }
 };
