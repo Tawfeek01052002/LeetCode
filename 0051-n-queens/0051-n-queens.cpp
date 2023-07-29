458d26671b4c2aec0a93a050ca4b904b6271599d
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-30 01:09:46 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (31.78%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    
+    unordered_map<int,bool> left;
+    
+    unordered_map<int,bool> lefttop;
+    
+    unordered_map<int,bool> leftbottom;
+    
     void saveAns(vector<vector<int>>& arr,int& n,vector<vector<string>>& ans){
         vector<string> tempstr;
         for(int i=0;i<n;i++){
@@ -14,6 +21,7 @@ public:
     
     bool isSafe(vector<vector<int>>& arr,int row,int col,int n){
         
+        /*
         int i=row;
         int j=col;
         
@@ -39,6 +47,13 @@ public:
             }
         }
         return true;
+        */
+        
+        // to optimise isSafe function we use map of O(1) time complexity
+        if(left[row] || lefttop[n-1+row-col] || leftbottom[row+col]){
+            return false;
+        }
+        return true;
     }
     
     void solve(vector<vector<int>>& arr,int& n,int col,vector<vector<string>>& ans){
@@ -47,13 +62,22 @@ public:
             return;
         }
         
-        for(int i=0;i<n;i++){
-            if(isSafe(arr,i,col,n)){
-                arr[i][col]='Q';
+        for(int row=0;row<n;row++){
+            if(isSafe(arr,row,col,n)){
+                
+                left[row]=true;
+                lefttop[n-1+row-col]=true;
+                leftbottom[row+col]=true;
+                
+                arr[row][col]='Q';
                 solve(arr,n,col+1,ans);
                 
                 //backtract condition
-                arr[i][col]='.';
+                arr[row][col]='.';
+                
+                left[row]=false;
+                lefttop[n-1+row-col]=false;
+                leftbottom[row+col]=false;
             }   
         }
     }
