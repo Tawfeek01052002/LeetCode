96c74c9f957aae82be84a4cd41ff011270e6437b
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-07-30 23:35:00 +0530

    Time: 26 ms (88.24%), Space: 6.3 MB (61.76%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isSafe(int row, int col, vector<vector<char>> &board, char value)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            // checking horizontally
+            if (board[row][i] == value)
+                return false;
+
+            if (board[i][col] == value)
+                return false;
+
+            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value)
+                return false;
+        }
+        return true;
+    }
+    
+    bool solve(vector<vector<char>> &board)
+    {
+        int n=board.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (board[i][j] == '.')
+                {
+                    for (char value = '1'; value <= '9'; value++)
+                    {
+                        if (isSafe(i, j, board, value))
+                        {
+                            board[i][j] = value;
+
+                            bool isWrong = solve(board);
+                            if (isWrong)
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+         solve(board);
+    }
+};
