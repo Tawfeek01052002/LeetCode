b820137a60af6055b4c0c3033c0688520be91f71
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-04 19:43:32 +0530

    Time: 6 ms (82.18%), Space: 10.9 MB (26.54%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     void allPerm(vector<int>& nums,int i,set<vector<int>>& ans){
         if(i>=nums.size()){
             ans.insert(nums);
@@ -11,14 +12,38 @@ public:
             swap(nums[i],nums[j]);
         }
         
+    }*/
+    
+    void allPerm(vector<int>& nums,int i,vector<vector<int>>& ans){
+        if(i>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        unordered_map<int,bool> visited;
+        for(int j=i;j<nums.size();j++){
+            if(visited.find(nums[j])!=visited.end()){
+                continue;
+            }
+            visited[nums[j]]=true;
+            
+            swap(nums[i],nums[j]);
+            allPerm(nums,i+1,ans);
+            swap(nums[i],nums[j]);
+        }
+        
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> ans;
+        /*set<vector<int>> ans;
         vector<vector<int>> out;
         allPerm(nums,0,ans);
         for (const vector<int>& v : ans) {
             out.push_back(v);
         }
         return out;
+        
+        */
+        vector<vector<int>> ans;
+        allPerm(nums,0,ans);
+        return ans;
     }
 };
