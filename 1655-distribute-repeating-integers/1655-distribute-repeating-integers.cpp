b4a3bd73b768a796af40a0bb8e1423cb2b45198d
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-04 23:20:51 +0530

    Time: 386 ms (91.69%), Space: 76.8 MB (44.18%) - LeetHub

---
+++ 1655-distribute-repeating-integers/1655-distribute-repeating-integers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canDistributeHelper(vector<int>& counts, vector<int>& quantity,int ithCustomer){
+        
+        //Base case
+        if(ithCustomer>=quantity.size()){
+            return true;
+        }
+        
+        for(int i=0;i<counts.size();i++){
+            if(counts[i]>=quantity[ithCustomer]){
+                counts[i]-=quantity[ithCustomer];
+                if(canDistributeHelper(counts,quantity,ithCustomer+1)){
+                    return true;
+                }
+                counts[i]+=quantity[ithCustomer];
+            }
+        }
+        
+        return false;
+        
+        
+        
+    }
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        vector<int> counts;
+        for(auto ele:m){
+            counts.push_back(ele.second);
+        }
+        int ithCustomer=0;
+        sort(quantity.rbegin(),quantity.rend());
+        return canDistributeHelper(counts,quantity,ithCustomer);
+    }
+};
