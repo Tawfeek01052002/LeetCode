77218f1288964c325acc37e7db2ed45b7cc9c79e
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-12 16:41:27 +0530

    Time: 37 ms (90.72%), Space: 14.8 MB (20.56%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a=headA;
+        ListNode* b=headB;
+        while(a->next!=NULL && b->next!=NULL){
+            if(a==b){
+                return a;
+            }
+            a=a->next;
+            b=b->next;
+        }
+        //b LL is bigger  find diff
+        if(a->next==NULL){
+            cout<<"A -- "<<endl;
+            int diff=0;
+            while(b->next!=NULL){
+                diff++;
+                b=b->next;
+            }
+            a=headA;
+            b=headB;
+            while(diff--){
+                b=b->next;
+            }
+        }
+        else{  // a LL is bigger find diff
+            int diff=0;
+            while(a->next!=NULL){
+                diff++;
+                a=a->next;
+            }
+            a=headA;
+            b=headB;
+            while(diff--){
+                a=a->next;
+            }
+        }
+        //after headA and headB of same lenght
+        while(a!=NULL && b!=NULL){
+            if(a==b){
+                return a;
+            }
+            a=a->next;
+            b=b->next;
+        }
+        return NULL;
+    }
+};
