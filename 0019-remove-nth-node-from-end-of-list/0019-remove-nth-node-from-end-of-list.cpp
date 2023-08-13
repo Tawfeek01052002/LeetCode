bcb3cd2372df566924c9c6a6a8dccdbfc01f2266
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-13 15:37:37 +0530

    Time: 2 ms (79.68%), Space: 10.6 MB (43.03%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,32 +10,53 @@
  */
 class Solution {
 public:
-    ListNode* reversewithCount(ListNode* &head,int& n,int& i){
-        if(head==NULL){
-            return NULL;
-        }
-        reversewithCount(head->next,n,i);
-        if(i==n){
-            if(head->next!=NULL){
-                head->next=head->next->next;
-            }
-        }
-        i++;
-        return head;
-    }
-    int getLength(ListNode *head){
-        int count=0;
-        while(head!=NULL){
-            head=head->next;
-            count++;
-        }
-        return count;
-    }
+    // ListNode* reversewithCount(ListNode* &head,int& n,int& i){
+    //     if(head==NULL){
+    //         return NULL;
+    //     }
+    //     reversewithCount(head->next,n,i);
+    //     if(i==n){
+    //         if(head->next!=NULL){
+    //             head->next=head->next->next;
+    //         }
+    //     }
+    //     i++;
+    //     return head;
+    // }
+    // int getLength(ListNode *head){
+    //     int count=0;
+    //     while(head!=NULL){
+    //         head=head->next;
+    //         count++;
+    //     }
+    //     return count;
+    // }
+    
+    
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int i=0;
-        if(n==getLength(head)){
-            return head->next;
+    //     int i=0;
+    //     if(n==getLength(head)){
+    //         return head->next;
+    //     }
+    //     return reversewithCount(head,n,i);
+
+    ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast->next;
         }
-        return reversewithCount(head,n,i);
+
+        while (fast != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return dummy->next;
     }
+
 };
