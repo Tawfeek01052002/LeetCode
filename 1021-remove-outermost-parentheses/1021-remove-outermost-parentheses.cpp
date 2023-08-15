b9da6963cfb5be400d6a365ad30c437e291222e2
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-15 18:58:24 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (41.90%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void removePara(string& str,stack<char>& s,int& i,string& ans){
+        if(i>=str.length()){
+            return;
+        }
+        if(str[i]=='('){
+            if(!s.empty()){
+                ans.push_back('(');
+            }
+            s.push(str[i]);
+        }
+        else{
+            s.pop();
+            if(!s.empty()){
+                ans.push_back(')');
+            }
+        }
+        i++;
+        removePara(str,s,i,ans);
+    }
+    string removeOuterParentheses(string str) {
+        int i=0;
+        stack<char> s;
+        string ans;
+        removePara(str,s,i,ans);
+        return ans;
+    }
+};
