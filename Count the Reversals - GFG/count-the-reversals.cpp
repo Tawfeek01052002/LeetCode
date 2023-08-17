b8e5bfbee40c5c1abb58c60c45d5240a55b59870
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-17 12:05:29 +0530

    Added solution - LeetHub

---
+++ Count the Reversals - GFG/count-the-reversals.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int countRev (string s);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        cout << countRev (s) << '\n';
+    }
+}
+
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int countRev (string str)
+{
+    int n=str.length();
+    if(n&1==1){
+        return -1;
+    }
+    
+    stack<char> s;
+
+    for(int i=0;i<n;i++){
+        char ch=str[i];
+        if(ch=='{'){
+            s.push(ch);
+        }
+        else if(ch=='}' && !s.empty() && s.top()=='{'){
+            s.pop();
+        }
+        else{
+            s.push(ch);
+        }
+    }
+    
+    int count=0;
+    while(!s.empty()){
+        char ch1=s.top();
+        s.pop();
+        char ch2=s.top();
+        s.pop();
+        
+        if(ch1==ch2){
+            count+=1;
+        }
+        else{
+            count+=2;
+        }
+    }
+    return count;
+}
