ce49c398aaa56fd9202ce4982d6a063d6c5cb06a
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-18 16:50:45 +0530

    Time: 3 ms (92.60%), Space: 9.1 MB (65.62%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> s;
+        int i=0;
+        while(i<path.size()){
+            
+            int start=i;
+            int end=i+1;
+            while(end<path.size() && path[end]!='/'){
+                end++;
+            }
+            
+            string str=path.substr(start,end-start);
+            i=end;
+            
+            if(str=="/"|| str=="/."){
+                continue;
+            }
+            
+            if(str!="/.."){
+                s.push(str);
+            }
+            else if(!s.empty()){
+                s.pop();
+            }
+        }
+        string ans=s.empty()?"/":"";
+        while(!s.empty()){
+            ans=s.top()+ans;
+            s.pop();
+        }
+        return ans;
+    }
+};
