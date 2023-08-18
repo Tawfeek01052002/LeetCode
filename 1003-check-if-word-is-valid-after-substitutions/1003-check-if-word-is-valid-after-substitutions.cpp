6fd33696749eb49ff3e8e9da48db079c5b4d8fe4
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-18 17:24:10 +0530

    Time: 3 ms (99.84%), Space: 8.3 MB (49.75%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,11 +1,48 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        int index=s.find("abc");
+    bool isValid(string str) {
+        
+        //working but very slow
+        //T.C.=O(n^2)
+        /*
+        int index=str.find("abc");
         while(index!=string::npos){
-            s.erase(index,3);
-            index=s.find("abc");
+            str.erase(index,3);
+            index=str.find("abc");
         }
         return s.size()==0?true:false;
+        
+        */
+        if(str[0]!='a') return false;
+        
+        stack<char> s;
+        for(char ch:str){
+            if(ch=='a'){
+                s.push(ch);
+            }
+            else if(ch=='b'){
+                if(!s.empty() &&s.top()=='a'){
+                    s.push(ch);
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(ch=='c'){
+                if(!s.empty() && s.top()=='b'){
+                    s.pop();
+                    if(!s.empty() && s.top()=='a'){
+                        s.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return s.empty();
     }
 };
