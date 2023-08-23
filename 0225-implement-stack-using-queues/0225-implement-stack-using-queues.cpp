030db9e0acb05a6630eba3fa58c47dee798e24ea
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-08-23 14:29:35 +0530

    Time: 3 ms (36.43%), Space: 6.9 MB (5.94%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,32 +1,42 @@
-class MyStack {
+class MyStack
+{
 public:
-    queue<int> q;
-    MyStack() {
-        
+    queue<int> q1, q2;
+    MyStack()
+    {
     }
-    
-    void push(int x) {
-        q.push(x);
-        
-        for(int i=0;i<q.size()-1;i++){
-            int x=q.front();
-            q.pop();
-            q.push(x);
+
+    void push(int x)
+    {
+        q2.push(x);
+
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        while (!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
         }
     }
-    
-    int pop() {
-        int topEle=q.front();
-        q.pop();
+
+    int pop()
+    {
+        int topEle = q1.front();
+        q1.pop();
         return topEle;
     }
-    
-    int top() {
-        return q.front();
+
+    int top()
+    {
+        return q1.front();
     }
-    
-    bool empty() {
-        return q.empty();
+
+    bool empty()
+    {
+        return q1.empty() && q2.empty();
     }
 };
 
