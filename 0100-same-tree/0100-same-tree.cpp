2557bec9afddeb84ba8a17bf395395f438327eb1
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2023-09-09 16:25:22 +0530

    Time: 2 ms (46.68%), Space: 10.1 MB (39.51%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,18 +14,29 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
         //Using Recursion
+        // if(p==NULL && q==NULL){
+        //     return true;
+        // }
+        // if((p==NULL && q!=NULL) || (p!=NULL && q==NULL)){
+        //     return false;
+        // }
+        // if(p->val ==q->val){
+        //     return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        // }
+        // else{
+        //     return false;
+        // }
+        
         if(p==NULL && q==NULL){
             return true;
         }
-        if((p==NULL && q!=NULL) || (p!=NULL && q==NULL)){
+        else if((p!=NULL && q==NULL )|| (p==NULL && q!=NULL)){
             return false;
         }
-        if(p->val ==q->val){
+        else if(p->val==q->val){
             return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
         }
-        else{
-            return false;
-        }
+        return false;
         
     }
 };
